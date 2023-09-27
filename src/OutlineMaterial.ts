50949b693dfe2166a16d203afa3cc2e81a84d52e
Daniel Keefe <dfk@umn.edu>
2023-09-26 20:35:47 -0500

    updated file headers

--- src/OutlineMaterial.ts
+++ src/OutlineMaterial.ts
@@ -1,4 +1,10 @@
-// CSCI 4611 Assignment 5: Artistic Rendering
+/* Assignment 5: Artistic Rendering
+ * UMN CSci-4611 Instructors 2012+
+ * GopherGfx implementation by Evan Suma Rosenberg <suma@umn.edu> 2022
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ * Please do not distribute beyond the CSci-4611 course
+ */ 
+
 // You only need to modify the shaders for this assignment.
 // You do not need to write any TypeScript code unless
 // you are planning to add wizard functionality.
